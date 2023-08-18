618423386076750668700fcf7afbfb48e3b9a42d
Robbert Langezaal <opicron@users.noreply.github.com>
2023-08-18 09:33:38 +0200

    Update mainwp-cli-extension.php
    implemented remove non-mainwp actions

--- mainwp-cli-extension.php
+++ mainwp-cli-extension.php
@@ -34,20 +34,19 @@ class MainWP_WP_CLI_Custom_Command extends MainWP_WP_CLI_Command {
 	 *
 	 * ## OPTIONS
 	 *
-	 * [--list]
-	 *  : Get a list of all child sites
+	 * [<websiteid>]
+	 * : The id (or ids, comma separated) of the child sites that need to be synced.
 	 *
-	 * [--count]
-	 *  : If set, count child sites.
-	 *
+	 * [--all]
+	 * : If set, all child sites will be synced.	 *
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     wp mainwp sites --list
-	 *     wp mainwp sites --all-sites-count
+	 *     wp mainwp delete_action 2,5
+	 *     wp mainwp delete_action --all
 	 *
-	 * ## Synopsis [--list] [--all-sites-count]
-	 *
+	 * ## Synopsis [<websiteid>] [--all]
+	 * 
 	 * @param array $args Function arguments.
 	 * @param array $assoc_args Function associate arguments.
 	 *
@@ -57,22 +56,41 @@ class MainWP_WP_CLI_Custom_Command extends MainWP_WP_CLI_Command {
 	 * @uses \MainWP\Dashboard\MainWP_DB::free_result()
 	 * @uses \MainWP\Dashboard\MainWP_DB::data_seek()
 	 */
-	public function test( $args, $assoc_args ) {
+	public function delete_actions( $args, $assoc_args ) {
 		
+		//todo: examine commented code functionality
 		// support new mainwp sites cli commands.
-		
-		$handle = MainWP_WP_CLI_Handle::get_assoc_args_commands( 'sites', $assoc_args );
+		/*		
+		$handle = MainWP_WP_CLI_Handle::get_assoc_args_commands( 'delete_actions', $assoc_args );
 		if ( ! empty( $handle ) ) {
-			MainWP_WP_CLI_Handle::handle_cli_callback( 'sites', $args, $assoc_args );
+			MainWP_WP_CLI_Handle::handle_cli_callback( 'delete_actions', $args, $assoc_args );
 			return;
 		}
+		*/
+		
+		$sites = array();
+		if ( count( $args ) > 0 ) {
+			$args_exploded = explode( ',', $args[0] );
+			foreach ( $args_exploded as $arg ) {
+				if ( ! is_numeric( trim( $arg ) ) ) {
+					\WP_CLI::error( 'Child site ids should be numeric.' );
+				}
 
+				$sites[] = trim( $arg );
+			}
+		}
+
+		if ( ( count( $sites ) == 0 ) && ( ! isset( $assoc_args['all'] ) ) ) {
+			\WP_CLI::error( 'Please specify one or more child sites, or use --all.' );
+		}
+		
 		$websites = MainWP_DB::instance()->query( MainWP_DB::instance()->get_sql_websites_for_current_user( false, null, 'wp.url', false, false, null, true ) );
-
-		$idLength      = strlen( 'id' );
+		
+		$idLength      = strlen( 'i' );
 		$nameLength    = strlen( 'name' );
 		$urlLength     = strlen( 'url' );
 		$versionLength = strlen( 'version' );
+		
 		while ( $websites && ( $website      = MainWP_DB::fetch_object( $websites ) ) ) {
 			if ( $idLength < strlen( $website->id ) ) {
 				$idLength = strlen( $website->id );
@@ -80,26 +98,54 @@ class MainWP_WP_CLI_Custom_Command extends MainWP_WP_CLI_Command {
 			if ( $nameLength < strlen( $website->name ) ) {
 				$nameLength = strlen( $website->name );
 			}
+			/*
 			if ( $urlLength < strlen( $website->url ) ) {
 				$urlLength = strlen( $website->url );
 			}
 			if ( $versionLength < strlen( $website->version ) ) {
 				$versionLength = strlen( $website->version );
 			}
+			*/
 		}
 		MainWP_DB::data_seek( $websites, 0 );
-
-		\WP_CLI::line( sprintf( "+%'--" . ( $idLength + 2 ) . "s+%'--" . ( $nameLength + 2 ) . "s+%'--" . ( $urlLength + 2 ) . "s+%'--" . ( $versionLength + 2 ) . 's+', '', '', '', '' ) );
-		\WP_CLI::line( sprintf( '| %-' . $idLength . 's | %-' . $nameLength . 's | %-' . $urlLength . 's | %-' . $versionLength . 's |', 'id', 'name', 'url', 'version' ) );
-		\WP_CLI::line( sprintf( "+%'--" . ( $idLength + 2 ) . "s+%'--" . ( $nameLength + 2 ) . "s+%'--" . ( $urlLength + 2 ) . "s+%'--" . ( $versionLength + 2 ) . 's+', '', '', '', '' ) );
-
+						
 		while ( $websites && ( $website = MainWP_DB::fetch_object( $websites ) ) ) {
-			\WP_CLI::line( sprintf( '| %-' . $idLength . 's | %-' . $nameLength . 's | %-' . $urlLength . 's | %-' . $versionLength . 's |', $website->id, $website->name, $website->url, $website->version ) );
+			
+			$success = false;
+			$error   = '';
+			
+			if ( ( count( $sites ) == 0 && ! isset( $assoc_args['all'] ) ) || ( !in_array( $website->id, $sites ) && ! isset( $assoc_args['all'] ) ) )
+				continue;
+			
+			try {
+				$response = MainWP_Connect::fetch_url_authed( $website, 'delete_actions', array( 'del' => 'act' ) );
+				if ( is_array( $response ) ) {
+					if ( isset( $response['success'] ) ) {
+						$success = true;
+					} elseif ( isset( $response['error'] ) ) {
+						$error = $response['error'];
+					}
+				}
+			} catch ( \Exception $e ) {
+				// ok!
+			}
+			if ( $success ) {
+				//MainWP_DB_Site_Actions::instance()->delete_action_by( 'wpid', $siteid );
+				//wp_die( wp_json_encode( array( 'success' => 'ok' ) ) );
+				//\WP_CLI::line( sprintf( '[%-' . $idLength . 's] %-' . $nameLength . 's - %-' . $urlLength . 's (%-' . $versionLength . 's) removed non-mainwp actions', $website->id, $website->name, $website->url, $website->version ) );
+				\WP_CLI::line( sprintf( '[%-' . $idLength . 's] %-' . $nameLength . 's :: removed non-mainwp actions', $website->id, $website->name ) );
+				continue;
+				
+			}
+			
+			if ( empty( $error ) ) {
+				\WP_CLI::line( sprintf( '%-' . $idLength . 's %-' . $nameLength . 's %-' . $urlLength . 's %-' . $versionLength . 's - undefined error, please try again', $website->id, $website->name, $website->url, $website->version ) );
+			}							
 		}
-
-		\WP_CLI::line( sprintf( "+%'--" . ( $idLength + 2 ) . "s+%'--" . ( $nameLength + 2 ) . "s+%'--" . ( $urlLength + 2 ) . "s+%'--" . ( $versionLength + 2 ) . 's+', '', '', '', '' ) );
-		MainWP_DB::free_result( $websites );
 		
+		MainWP_DB::free_result( $websites );
+				
+		//wp_die( wp_json_encode( array( 'error' => $error ) ) );
 	}
 }
 
